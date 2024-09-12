17b8844bfb764a82bb4f0248d78670c0a1e9046f
hanapiko <apikojuma94@gmail.com>
2024-09-12 10:23:05 +0300

    chore: add example of input to print

--- main.go
+++ main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+    "fmt"
+    "kisumu/lexer" 
+)
+
 func main() {
+    input := `func main() {
+        display("Hello, World!");
+        x := 5 + 10;
+    }`
+
+    l := lexer.NewLexer(input)
+    for {
+        tok := l.NextToken()
+        fmt.Printf("Token Type: %d, Literal: %q\n", tok.Type, tok.Literal)
+
+        if tok.Type == lexer.EOF {
+            break
+        }
+    }
+    // fmt.Println(l)
 }
